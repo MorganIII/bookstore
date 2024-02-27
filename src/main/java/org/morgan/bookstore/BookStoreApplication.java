8669aefbc6baed30386c3e087289391044397431
MorganIII <mosaab.morgan@gmail.com>
2024-02-27 12:41:21 +0200

    - Added stripe-java dependency.
    - added stripe library to handle online payments
    
    - updated application.yml and added stripe public, secret and webhook keys.
    
    - updated BookStoreApplication.java to and added the setup method to initialize the stripe secret key during the initialization of the application.

--- src/main/java/org/morgan/bookstore/BookStoreApplication.java
+++ src/main/java/org/morgan/bookstore/BookStoreApplication.java
@@ -1,5 +1,8 @@
 package org.morgan.bookstore;
 
+import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,8 +13,15 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class BookStoreApplication {
 
+    @Value("${stripe.secret-key}")
+    private String stripeSecretKey;
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setup() {
+        Stripe.apiKey = stripeSecretKey;
     }
 
 
