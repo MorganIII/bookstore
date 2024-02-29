fa5eb453122698b95647d769946dad125671faca
MorganIII <mosaab.morgan@gmail.com>
2024-02-29 11:03:47 +0200

    Added constructor for all fields except subTotal field

--- src/main/java/org/morgan/bookstore/response/ItemDTO.java
+++ src/main/java/org/morgan/bookstore/response/ItemDTO.java
@@ -1,11 +1,13 @@
 package org.morgan.bookstore.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -26,4 +28,13 @@ public class ItemDTO {
     private Double subTotal;
 
     private String bookThumbnail;
+
+    public ItemDTO(Integer itemId, Integer bookId, String title, Double actualPrice, Integer quantity, String bookThumbnail) {
+        this.itemId = itemId;
+        this.bookId = bookId;
+        this.title = title;
+        this.actualPrice = actualPrice;
+        this.quantity = quantity;
+        this.bookThumbnail = bookThumbnail;
+    }
 }
